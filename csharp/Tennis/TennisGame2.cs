3ad1ff809e7dd22b5afc38229bbb5e0977cff781
RomFAN <kireevrom@gmail.com>
2024-01-28 18:41:15 +0100

    Getting rid of repetitive code in GetScore()
    The first thing caught my eye - a lot of repetitive code in GetScore(), which can be replaced by one function with switch statement.

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -22,12 +22,7 @@ namespace Tennis
             var score = "";
             if (p1point == p2point && p1point < 3)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
+                score = ConvertPointsToString(p1point);  
                 score += "-All";
             }
             if (p1point == p2point && p1point > 2)
@@ -35,51 +30,27 @@ namespace Tennis
 
             if (p1point > 0 && p2point == 0)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
+                p1res = ConvertPointsToString(p1point);  
                 p2res = "Love";
                 score = p1res + "-" + p2res;
             }
             if (p2point > 0 && p1point == 0)
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
+                p2res = ConvertPointsToString(p2point);  
                 p1res = "Love";
                 score = p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
+                p2res = ConvertPointsToString(p2point);   
+                p1res = ConvertPointsToString(p1point);  
                 score = p1res + "-" + p2res;
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
+                p2res = ConvertPointsToString(p2point);  
+                p1res = ConvertPointsToString(p1point);            
                 score = p1res + "-" + p2res;
             }
 
@@ -136,6 +107,18 @@ namespace Tennis
                 P1Score();
             else
                 P2Score();
+        }
+
+        private string ConvertPointsToString(int points)
+        {
+            return points switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => "",
+            };
         }
 
     }
